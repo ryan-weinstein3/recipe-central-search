b4ef5cef8515c733db15de696256cab04af39216
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-13 17:11:41 -0500

    Update search_recipes.js

--- search_recipes.js
+++ search_recipes.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 var port = process.env.PORT || 3000;
 let db_url = "mongodb+srv://Jaysonpit:Giamo@cluster0.jfe6e.mongodb.net/Finaldb?retryWrites=true&w=majority"
 
-<link rel="stylesheet" href="RecipeCentral.css" />
+//<link rel="stylesheet" href="RecipeCentral.css" />
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {
