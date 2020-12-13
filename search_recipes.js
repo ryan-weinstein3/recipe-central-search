5dc47173820500bf1050fdce7dd94f92eb703c2e
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-13 18:27:59 -0500

    Update search_recipes.js

--- search_recipes.js
+++ search_recipes.js
@@ -15,36 +15,4 @@ var server = app.listen((process.env.PORT || 3000), function() {
     console.log('listening to port'); 
 }); 
 
-/*http.createServer(function (req, res) {
-	//app.use(express.static("public"));
-	app.set('view engine', 'ejs');
-	app.get('/', function(req, res) {
-    		res.render('home');
-	});
-	//res.writeHead(200, {
-    	//	'Content-Type': 'text/html'});
-	//res.write("<h1>Query Result</h1>");
-	/*var qobj = url.parse(req.url, true).query;
-	var query_string = qobj.query;
 
-    MongoClient.connect(db_url, {useUnifiedTopology: true}, async function(err, db) {
-	    try {
-		    if(err) { return console.log(err); }
-		    var dbo = db.db("Finaldb");
-			var collection = dbo.collection('Recipes');
-			var query = {strMeal: query_string};
-
-			await collection.find(query, {projection: {strMeal: 1, strInstructions: 1}}).toArray(function (err, result) {
-				if (err) throw err;
-				var result_str = JSON.stringify(result, null, 2);
-				res.write(result_str);
-			})
-			await db.close();
-			await res.end();
-		}
-		catch(err) {
-			console.log(err);
-		}
- 
-	});
-}).listen(port);
