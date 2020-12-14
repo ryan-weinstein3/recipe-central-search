90d32b2e96264360ef794cd175b8b315dfa1cecd
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-13 19:15:22 -0500

    Update search_recipes.js

--- search_recipes.js
+++ search_recipes.js
@@ -18,17 +18,17 @@ res.render('home');
     MongoClient.connect(db_url, {useUnifiedTopology: true}, async function(err, db) {
 	    try {
 		    if(err) { return console.log(err); }
-		    /*var dbo = db.db("Finaldb");
+		    var dbo = db.db("Finaldb");
 			var collection = dbo.collection('Recipes');
 			var query = {strMeal: query_string};
 
-			await collection.find(query, {projection: {strMeal: 1, strInstructions: 1}}).toArray(function (err, result) {
+			/*await collection.find(query, {projection: {strMeal: 1, strInstructions: 1}}).toArray(function (err, result) {
 				if (err) throw err;
 				var result_str = JSON.stringify(result, null, 2);
 				res.write(result_str);
-			})
+			})*/
 			await db.close();
-			await res.end();*/
+			await res.end();
 		}
 		catch(err) {
 			console.log(err);
