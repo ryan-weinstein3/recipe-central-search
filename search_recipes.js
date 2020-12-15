de811d5d70689d08b17dcfb76a9d77fb1ab27636
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-14 19:01:24 -0500

    Update search_recipes.js

--- search_recipes.js
+++ search_recipes.js
@@ -124,7 +124,7 @@ app.get('/SendEmail', (req, res)=>{
 		user: 'Recipecent@gmail.com',
 		pass: 'RecipeYum',
 		}
-	});
+	});*/
 
 	var mailOptions = {
 	    from: 'Recipe-Central',
@@ -133,7 +133,7 @@ app.get('/SendEmail', (req, res)=>{
 	    text: 'Your information has been confirmed. Thank you for joining the Recipe Central community!'
 	};
 
-	transporter.sendMail(mailOptions, function(error, info) {
+	/*transporter.sendMail(mailOptions, function(error, info) {
 	    if(error){
 		console.log(error);
 	    } else {
